2cd07e9822b95bf216882d8a2be241b411459bca
QinghanHuang <darvish841@gmail.com>
2023-06-22 14:47:06 +0100

    Initial commit

---
+++ FrameWork-Backend/src/main/java/com/clement/FrameWorkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameWorkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameWorkBackendApplication.class, args);
+    }
+
+}
